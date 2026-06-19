ae4de68fcb90d430ca37e54ca5dd2e95fd60f56f
anade-mo <anade-mo@c3r5s7.42roma.it>
2026-06-19 14:01:18 +0200

    fix: input questions

--- ps_input/src/gen_target.c
+++ ps_input/src/gen_target.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   gen_target.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anade-mo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: anade-mo <anade-mo <marvin@42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 18:05:40 by anade-mo          #+#    #+#             */
-/*   Updated: 2026/06/14 22:59:12 by anade-mo         ###   ########.fr       */
+/*   Updated: 2026/06/19 14:00:19 by anade-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,20 +57,20 @@ int	get_algorithm(t_gen *g)
 	g->level = -1;
 	while (g->level == -1)
 	{
-		ft_printf("Which algorithm?\n(adaptive | simple | medium | complex)\n");
+		ft_printf("Which algorithm?\n(adaptive 0 | simple 1| medium 2| complex 3)\n");
 		line = get_next_line(0);
 		if (!line)
 			return (0);
-		if (!ft_strncmp(line, "adaptive\n", 9))
+		if (!ft_strncmp(line, "0\n", 2))
 			g->level = 0;
-		else if (!ft_strncmp(line, "simple\n", 7))
+		else if (!ft_strncmp(line, "1\n", 2))
 			g->level = 1;
-		else if (!ft_strncmp(line, "medium\n", 7))
+		else if (!ft_strncmp(line, "2\n", 2))
 			g->level = 2;
-		else if (!ft_strncmp(line, "complex\n", 8))
+		else if (!ft_strncmp(line, "3\n", 2))
 			g->level = 3;
 		else
-			ft_printf("Invalid: enter adaptive, simple, medium or complex\n");
+			ft_printf("Invalid: enter 0, 1, 2 or 3\n");
 		free(line);
 	}
 	return (1);
@@ -105,16 +105,16 @@ int	get_rank_or_value(t_gen *g)
 	g->use_rank = -1;
 	while (g->use_rank == -1)
 	{
-		ft_printf("Print rank or value (random ints)?\n(rank / value)\n");
+		ft_printf("Print rank or value?\n(r for rank (0 - n-1) | v for value (random int_min - int_max)\n");
 		line = get_next_line(0);
 		if (!line)
 			return (0);
-		if (!ft_strncmp(line, "rank\n", 5))
+		if (!ft_strncmp(line, "r\n", 2))
 			g->use_rank = 1;
-		else if (!ft_strncmp(line, "value\n", 6))
+		else if (!ft_strncmp(line, "v\n", 2))
 			g->use_rank = 0;
 		else
-			ft_printf("Invalid: enter rank or value\n");
+			ft_printf("Invalid: enter r or v\n");
 		free(line);
 	}
 	return (1);
